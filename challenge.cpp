66767b9732d6f61af784e89aa4a1348cf4932e5b
tegartamburaka9-svg <tegartamburaka9@gmail.com>
2026-03-02 16:05:10 +0700

    membuat float phi

--- challenge.cpp
+++ challenge.cpp
@@ -4,26 +4,3 @@ using namespace std;
 
 float phi = 3.14159;
     int r;
-
-void input() {
-    cout << "Masukkan luas: ";
-    cin >> r; 
-}
-
-float luasLingkaran(float r) {
-    return phi * r * r;
-}
-float jumlah(float phi, float r) {
-    return phi * r * r;
-
-}
-
-void output() {
-    cout << "Hasilnya : " << luasLingkaran(r) << endl;
-}
-
-int main() {
-    input();
-    output();
-    cout << "Hasil dari penjumlahan : " << jumlah(phi, r) << endl;
-}
